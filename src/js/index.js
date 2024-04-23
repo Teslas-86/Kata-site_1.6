a08ebf68c1673c12cb2e44505026119731071a93
Aleksandr Kiselev <sasha.kiselev0904@mail.ru>
2024-04-23 21:00:29 +0500

    немного того, немого сего

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,49 @@
 import '../scss/style.scss'
+import resizableSwiper from './resizable-swiper'
+import Swiper from 'swiper'
 
-console.log('Works!')
+window.addEventListener('DOMContentLoaded', () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper
+
+    breakpoint = window.matchMedia(breakpoint)
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings)
+
+      if (callback) {
+        callback(swiper)
+      }
+    }
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings)
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true)
+        return
+      }
+    }
+
+    breakpoint.addEventListener('change', checker)
+    checker()
+  }
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on('slideChange', function (e) {
+        console.log('*** mySwiper.activeIndex', instance.activeIndex)
+      })
+    }
+  }
+
+  resizableSwiper('(max-width: 1100px)', '.nav-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+  })
+})
